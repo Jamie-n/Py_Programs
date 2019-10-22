a121564936be48296e5802c7c1e882951b808f87
Jamie <u1854720@unimail.hud.ac.uk>
2019-10-23 00:55:22 +0100

    Update 2

--- testMarkingUtility.py
+++ testMarkingUtility.py
@@ -6,14 +6,15 @@ import sys
 NUMBER_OF_QUESTIONS = 4
 STARTING_QUESTION = 1
 run_once = False
+pupil_name = ""
 
 #Calculates the scores of the student
-def display_Scores(scores_list):
-    print("The highest score for this pupil is " + str(max(scores_list)))
-    print("The lowest score for this pupil is " + str(min(scores_list)))
-    print("The average score for this pupil is " + str(sum(scores_list) / NUMBER_OF_QUESTIONS))
+def display_Scores(scores_list, student_name):
+    print("The highest score for "+student_name+" is " + str(max(scores_list)))
+    print("The lowest score for "+student_name+" is " + str(min(scores_list)))
+    print("The average score for "+student_name+" is " + str(sum(scores_list) / NUMBER_OF_QUESTIONS))
 
-    process_again = input("Do you want to process more scores y/n?")
+    process_again = input("Do you want to process more scores y/n? \n")
     if yes_or_no(process_again):
         add_Scores([], 1)
     else:
@@ -23,16 +24,12 @@ def display_Scores(scores_list):
 
 #Module for user input - takes number of qustions as arguement
 def add_Scores(marks_Scored, x):
-    print(marks_Scored)
-
-
     another_question = True
     while another_question:
-            current_score = (input("What did the pupil score on question "+ str(x) + "?"))
+            current_score = (input("What did the pupil score on question "+ str(x) + "? \n"))
             if current_score.isdigit():
                 marks_Scored.append(current_score)
             else:
-                another_question == False
                 escape_function(marks_Scored)
             time.sleep(0.2)
             x += 1
@@ -48,9 +45,10 @@ def escape_function(marks_Scored):
         add_another_score = input("Are you sure that you dont want to add another score to the test? Y/N \n").lower().strip()
         print(marks_Scored)
         if yes_or_no(add_another_score):
-            display_Scores(marks_Scored)
+            ask_for_name()
+            display_Scores(marks_Scored, pupil_name)
         else:
-            add_Scores(marks_Scored, len(marks_Scored))
+            add_Scores(marks_Scored, len(marks_Scored)+1)
 
 def yes_or_no(user_answer):
     user_answer.strip().lower()
@@ -70,9 +68,12 @@ def on_start():
         print("Here we go")
         time.sleep(1)
 
+def ask_for_name():
+   global pupil_name
+   pupil_name= input("What is the name of this pupil? \n")
+
 
 on_start()
-
 
 
 
